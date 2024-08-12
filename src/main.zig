d0eb2f0dfe907de9e090fce604938ec097563c4c
Craig <craig.collier@ro.co>
2024-08-12 16:32:32 -0700

    wip

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,9 @@ const material = @import("material.zig");
 const stdout = std.io.getStdOut().writer();
 const stderr = std.io.getStdErr().writer();
 
+var xoshiro = std.rand.DefaultPrng.init(0);
+const random = xoshiro.random();
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
@@ -20,75 +23,116 @@ pub fn main() !void {
     const world = w._hittable();
 
     var material_ground = material.Lambertian{
-        .albedo = color.Color.init(0.8, 0.8, 0.0),
+        .albedo = color.Color.init(0.5, 0.5, 0.5),
     };
-
-    var material_center = material.Lambertian{
-        .albedo = color.Color.init(0.1, 0.2, 0.5),
-    };
-
-    var material_left = material.Dialectric{
-        .refraction_index = 1.50,
-    };
-
-    var material_right = material.Metal.init(
-        color.Color.init(0.8, 0.6, 0.2),
-        0.0,
-    );
 
     var ground_sphere = try alloc.create(sphere.Sphere);
     defer alloc.destroy(ground_sphere);
     ground_sphere.* = sphere.Sphere{
-        .center = vec3.Point3.init(0, -100.5, -1),
-        .radius = 100,
+        .center = vec3.Point3.init(0, -1000, 0),
+        .radius = 1000,
         .mat = material_ground._material(),
     };
     var gs = ground_sphere._hittable();
-
-    var center_sphere = try alloc.create(sphere.Sphere);
-    defer alloc.destroy(center_sphere);
-    center_sphere.* = sphere.Sphere{
-        .center = vec3.Point3.init(0, 0, -1.2),
-        .radius = 0.5,
-        .mat = material_center._material(),
-    };
-    var cs = center_sphere._hittable();
-
-    var left_sphere = try alloc.create(sphere.Sphere);
-    defer alloc.destroy(left_sphere);
-    left_sphere.* = sphere.Sphere{
-        .center = vec3.Point3.init(-1, 0, -1),
-        .radius = 0.5,
-        .mat = material_left._material(),
-    };
-    var ls = left_sphere._hittable();
-
-    var right_sphere = try alloc.create(sphere.Sphere);
-    defer alloc.destroy(right_sphere);
-    right_sphere.* = sphere.Sphere{
-        .center = vec3.Point3.init(1, 0, -1),
-        .radius = 0.5,
-        .mat = material_right._material(),
-    };
-    var rs = right_sphere._hittable();
-
     try w.add(&gs);
-    try w.add(&cs);
-    try w.add(&ls);
-    try w.add(&rs);
+
+    var a: i32 = -11;
+    while (a < 11) : (a += 1) {
+        var b: i32 = -11;
+        while (b < 11) : (b += 1) {
+            const choose_mat = random.float(f32);
+            const center = vec3.Point3.init(@as(f32, @floatFromInt(a)) + 0.9 * random.float(f32), 0.2, @as(f32, @floatFromInt(b)) + 0.9 * random.float(f32));
+            var random_sphere = try alloc.create(sphere.Sphere);
+
+            if ((center.sub(vec3.Point3.init(4, 0.2, 0)).len() > 0.9)) {
+                if (choose_mat < 0.8) {
+                    const albedo = color.Color.rand();
+                    var mat = material.Lambertian{
+                        .albedo = albedo,
+                    };
+                    random_sphere.* = sphere.Sphere{
+                        .center = center,
+                        .radius = 0.2,
+                        .mat = mat._material(),
+                    };
+                } else if (choose_mat < 0.95) {
+                    const albedo = color.Color.rand_range(0.5, 1.0);
+                    const fuzz = random.float(f32) * 0.5;
+                    var mat = material.Metal{
+                        .albedo = albedo,
+                        .fuzz = fuzz,
+                    };
+                    random_sphere.* = sphere.Sphere{
+                        .center = center,
+                        .radius = 0.2,
+                        .mat = mat._material(),
+                    };
+                } else {
+                    var mat = material.Dialectric{
+                        .refraction_index = 1.5,
+                    };
+                    random_sphere.* = sphere.Sphere{
+                        .center = center,
+                        .radius = 0.2,
+                        .mat = mat._material(),
+                    };
+                }
+                var s = random_sphere._hittable();
+                try w.add(&s);
+            }
+        }
+    }
+
+    var material1 = material.Dialectric{
+        .refraction_index = 1.5,
+    };
+    var sphere1 = try alloc.create(sphere.Sphere);
+    sphere1.* = sphere.Sphere{
+        .center = vec3.Point3.init(0, 1, 0),
+        .radius = 1.0,
+        .mat = material1._material(),
+    };
+    var s1 = sphere1._hittable();
+    try w.add(&s1);
+
+    var material2 = material.Lambertian{
+        .albedo = color.Color.init(0.4, 0.2, 0.1),
+    };
+    var sphere2 = try alloc.create(sphere.Sphere);
+    sphere2.* = sphere.Sphere{
+        .center = vec3.Point3.init(-4, 1, 0),
+        .radius = 1.0,
+        .mat = material2._material(),
+    };
+    var s2 = sphere2._hittable();
+    try w.add(&s2);
+
+    var material3 = material.Metal{
+        .albedo = color.Color.init(0.7, 0.6, 0.5),
+        .fuzz = 0.5,
+    };
+    var sphere3 = try alloc.create(sphere.Sphere);
+    sphere3.* = sphere.Sphere{
+        .center = vec3.Point3.init(4, 1, 0),
+        .radius = 1.0,
+        .mat = material3._material(),
+    };
+    var s3 = sphere3._hittable();
+    try w.add(&s3);
 
     var cam = camera.Camera{};
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.img_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.img_width = 1200;
+    cam.samples_per_pixel = 500;
     cam.max_depth = 50;
+
     cam.vfov = 20;
-    cam.lookfrom = vec3.Point3.init(-2, 2, 1);
-    cam.lookat = vec3.Point3.init(0, 0, -1);
+    cam.lookfrom = vec3.Point3.init(13, 2, 3);
+    cam.lookat = vec3.Point3.init(0, 0, 0);
     cam.vup = vec3.Vec3.init(0, 1, 0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
 
     try cam.render(&world);
 }
